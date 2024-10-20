3b400f14c29c5b60012711a29698d79ddf538536
yunusemrekayis <104589120+yunusemrekayis@users.noreply.github.com>
2024-10-20 10:00:01 +0000

    başlangıç python projesinin ortama yüklenmesi

---
+++ hello.py
@@ -0,0 +1,14 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# Bu mantıklı bir kod olmadığı için uyarı yaratır
+# var = 1
+# var = var
+
+# Kötü syntax
+# foo =
